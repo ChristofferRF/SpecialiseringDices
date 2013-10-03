5cd18e7a6177a6b30b7e5aecc235398b59aaffa1
Christoffer Finn Nielsen <chromecrf@gmail.com>
2013-10-03 09:41:46 +0200

    flyttet mapper og opdateret til GIT

---
+++ Thirty3D/Assets/TheNotes.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TheNotes : MonoBehaviour {
+	
+	//make a background box
+	void OnGUI()
+	{
+		GUI.Box (Rect (10,10,100,90), "Loader Menu");
+		
+		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+	if (GUI.Button (Rect (20,40,80,20), "Level 1")) {
+		Application.LoadLevel (1);
+	}
+
+	// Make the second button.
+	if (GUI.Button (Rect (20,70,80,20), "Level 2")) {
+		Application.LoadLevel (2);
+	}
+	
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
