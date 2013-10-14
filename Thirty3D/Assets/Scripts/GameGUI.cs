99790196d9ef03a02b6e5299dcb02a1f8923b71a
Christoffer Finn Nielsen <chromecrf@gmail.com>
2013-10-14 18:40:58 +0200

    all the things

---
+++ Thirty3D/Assets/Scripts/GameGUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameGUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnGUI()
+	{
+		GUI.BeginGroup(new Rect(0,0, Screen.width, Screen.height));
+		if(GUI.Button(new Rect(Screen.width/2 + 150,Screen.height/2,100,50), "Exit game"))
+			Application.Quit();
+		if(GUI.Button(new Rect(Screen.width/2 + 150,Screen.height/2 + 50,100,50), "Back to Main"))
+			Application.LoadLevel(0);
+		GUI.EndGroup();
+	}
+	
+}
---
+++ Thirty3D/Assets/Scripts/StartMainMenu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnGUI()
+	{
+		GUI.BeginGroup(new Rect(0, 0, Screen.width,Screen.height));
+			GUI.Box(new Rect(0, 0, Screen.width,Screen.height), "Main Menu");
+			if(GUI.Button(new Rect(0 + Screen.width/2, 0 + Screen.height/2, 125, 50), "Start Game"))
+			{
+				Application.LoadLevel ("sceneProto"); 
+			}
+		if(GUI.Button(new Rect(0 + Screen.width/2, (0 + Screen.height/2) + 60, 125, 50), "Exit Game"))
+		{
+			Application.Quit();
+		}
+		GUI.EndGroup();
+	}
+}
